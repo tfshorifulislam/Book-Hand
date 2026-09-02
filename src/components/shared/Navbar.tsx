320c3176595268177016c3f10bc64624c050a0e5
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-03 02:06:44 +0600

    navbar update

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -41,17 +41,22 @@ export function NavigationBar() {
                     </div>
 
 
-                    <div className="absolute left-1/2 hidden w-full max-w-sm -translate-x-1/2 md:block lg:max-w-md">
-                        <div className="relative">
-                            <Search className="absolute left-3 top-1/2 size-4 -translate-y-1/2 text-muted-foreground" />
+                    {
+                        user &&
+                        <>
+                            <div className="absolute left-1/2 hidden w-full max-w-sm -translate-x-1/2 md:block lg:max-w-md">
+                                <div className="relative">
+                                    <Search className="absolute left-3 top-1/2 size-4 -translate-y-1/2 text-muted-foreground" />
 
-                            <Input
-                                type="search"
-                                placeholder="Search books, authors..."
-                                className="h-9 rounded-lg border-muted bg-muted/40 pl-9 pr-4 text-sm shadow-none transition-all focus-visible:bg-background focus-visible:ring-1"
-                            />
-                        </div>
-                    </div>
+                                    <Input
+                                        type="search"
+                                        placeholder="Search books, authors..."
+                                        className="h-9 rounded-lg border-muted bg-muted/40 pl-9 pr-4 text-sm shadow-none transition-all focus-visible:bg-background focus-visible:ring-1"
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    }
 
                     {/* Right */}
                     <div className="flex items-center gap-3">
