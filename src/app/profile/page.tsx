ec199bd90e9c40a560d58e3e2748d25d0cf8573b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-10 15:08:56 +0600

    proile page design done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,41 +9,42 @@ const ProfilePage = async () => {
     });
 
     const user = userInfo?.user;
-    console.log(user);
 
     return (
-       <div className="mx-auto my-5 max-w-7xl px-4 pt-3 md:my-20 md:px-6">
-    {/* Cover + Avatar */}
-    <div className="relative w-full">
-        <Image
-            width={2000}
-            height={500}
-            src="/cover.png"
-            alt="Profile cover"
-            className="aspect-4/1 w-full rounded-lg object-cover md:rounded-xl"
-        />
+        <div className="mx-auto my-5 max-w-7xl px-4 pt-3 md:px-6">
+           
+            <div className="relative w-full">
+                <Image
+                    width={2000}
+                    height={500}
+                    src="/cover.png"
+                    alt="Profile cover"
+                    className="aspect-4/1 w-full rounded-lg object-cover md:rounded-xl"
+                />
 
-        <div className="absolute bottom-0 left-1/2 z-50 size-20 -translate-x-1/2 translate-y-1/2 overflow-hidden rounded-full border-2 border-background bg-background md:size-30 md:border-4">
-            <Image
-                fill
-                alt="Profile image"
-                src={user?.image || "/profileAvatar.png"}
-                className="object-cover"
-            />
+                <div className="absolute bottom-0 left-1/2 z-50 size-20 -translate-x-1/2 translate-y-1/2 overflow-hidden rounded-full border-2 border-background bg-background md:size-30 md:border-4">
+                    <Image
+                        fill
+                        alt="Profile image"
+                        src={user?.image || "/profileAvatar.png"}
+                        className="object-cover"
+                    />
+                </div>
+            </div>
+          
+
+            <div className="mt-14 flex flex-col items-center justify-center text-center md:mt-20">
+                <h1 className="text-2xl font-semibold tracking-tight md:text-3xl">
+                    {user?.name}
+                </h1>
+
+                <p className="mt-1 text-sm text-muted-foreground md:text-base">
+                    {user?.email}
+                </p>
+            </div>
+
+
         </div>
-    </div>
-
-    {/* Profile Info */}
-    <div className="mt-14 flex flex-col items-center justify-center text-center md:mt-20">
-        <h1 className="text-2xl font-semibold tracking-tight md:text-3xl">
-            {user?.name}
-        </h1>
-
-        <p className="mt-1 text-sm text-muted-foreground md:text-base">
-            {user?.email}
-        </p>
-    </div>
-</div>
     );
 };
 
