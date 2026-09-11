23e841adc850b687a5d2ddf0056fb8a07a68ad35
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-12 03:00:22 +0600

    book details page loadin done

---
+++ src/app/books/[id]/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex min-h-[60vh] items-center justify-center mx-auto">
+            <div className="size-8 animate-spin rounded-full border-4 border-muted border-t-emerald-500" />
+        </div>
+    );
+};
+
+export default Loading;
