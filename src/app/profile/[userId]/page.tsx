6aaa85efa67266676df039a0e6ed38f49d813b8b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-14 01:09:42 +0600

    update get profile user id

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -5,28 +5,35 @@ import CoverProfile from "@/components/Profile_Components/Cover_Profile";
 
 type Props = {
     params: Promise<{
-        userId: string
-    }>
-}
-const UsserIdProfilePage = async ({ params }: Props) => {
+        userId: string;
+    }>;
+};
 
+const UserIdProfilePage = async ({ params }: Props) => {
     const { userId } = await params;
-    const userInfo = await userProfile(userId);
-    const user = userInfo?.user;
 
-    const item = await getUserBooks(userId);
+    const [userInfo, booksData] = await Promise.all([
+        userProfile(userId),
+        getUserBooks(userId),
+    ]);
+
+    const user = userInfo?.user;
+    const books = booksData?.listings ?? [];
 
     return (
-        <div className="mx-auto">
-            <CoverProfile
-                user={user}
-            />
+        <div className="mx-auto max-w-7xl">
+            <CoverProfile user={user} />
 
-            <BooksCard
-                item={item}
-            />
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {books.map((item) => (
+                    <BooksCard
+                        key={item.id}
+                        item={item}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
 
-export default UsserIdProfilePage;
+export default UserIdProfilePage;
