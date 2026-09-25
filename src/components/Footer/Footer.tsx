812f5358478c3ec5be59f66aceca769f204808c5
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-25 15:43:11 +0600

    footer section done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,13 +5,13 @@ import { FooterBottom } from "./FooterBottom";
 export function Footer() {
   return (
     <footer className="border-t bg-background">
-      <div className="mx-auto w-full max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
-        <div className="grid gap-10 md:grid-cols-[1.5fr_1fr]">
+      <div className="mx-auto max-w-370 px-6 py-14 lg:px-8">
+        <div className="grid gap-12 md:grid-cols-[1.4fr_1fr] lg:gap-20">
           <FooterBrand />
           <FooterLinks />
         </div>
 
-        <div className="mt-10">
+        <div className="mt-12 border-t pt-6">
           <FooterBottom />
         </div>
       </div>
